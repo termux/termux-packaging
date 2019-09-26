5874e6c6f04ba867da248082a2913bc8b6aebcb7
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-26 23:42:23 +0300

    generate-bootstraps.sh: reduce bootstrap archive size by excluding optional parts

--- scripts/generate-bootstraps.sh
+++ scripts/generate-bootstraps.sh
@@ -323,13 +323,13 @@ for package_arch in "${TERMUX_ARCHITECTURES[@]}"; do
 
 	# Additional.
 	pull_package busybox
-	pull_package ed
-	pull_package dos2unix
-	pull_package inetutils
-	pull_package net-tools
-	pull_package patch
-	pull_package unzip
-	pull_package util-linux
+	#pull_package ed
+	#pull_package dos2unix
+	#pull_package inetutils
+	#pull_package net-tools
+	#pull_package patch
+	#pull_package unzip
+	#pull_package util-linux
 
 
 	# Handle additional packages.
